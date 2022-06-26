1423129c43f910c4125dbf6b11ced0c8844fadaa
Abbbyccc <abbychien@hotmail.com>
2022-06-26 23:46:39 +0100

    updae

--- src/log.js
+++ src/log.js
@@ -1,6 +1,6 @@
 const registerForm = document.querySelector('#registerForm')
 
-
+register()
 function signup(event) {
     event.preventDefault()
     const fname = document.querySelector('#fName').value
@@ -9,7 +9,6 @@ function signup(event) {
     if (!/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/.test(dob)) {
         alert('please enter date of birth in dd/mm/yyyy')
     } else {
-
         let studentData = JSON.parse(localStorage.getItem('studentData')) || [];
         let exist = studentData.length && JSON.parse(localStorage.getItem('studentData')).some(data => data.fname == fname && data.lname == lname && data.dob == dob)
         if (!exist) {
@@ -23,7 +22,6 @@ function signup(event) {
             alert("Student already exists, please check again");
 
         }
-
     }
 }
 
@@ -31,11 +29,10 @@ function makeList(fname, lname, dob) {
     let list = [fname, lname, dob]
     let table = document.querySelector('.studentTable')
     let tableRow = document.createElement("tr")
+    tableRow.setAttribute('class', 'studentData')
     let registerButton = document.createElement("button")
     registerButton.setAttribute('class', 'register-btn')
-    registerButton.setAttribute('onclick', 'register()')
     registerButton.innerHTML = 'Register'
-
     list.forEach((el) => {
         let tableData = document.createElement("td")
         tableData.textContent = el
@@ -43,5 +40,20 @@ function makeList(fname, lname, dob) {
     })
     tableRow.appendChild(registerButton)
     table.appendChild(tableRow)
+    register()
 }
 
+
+function register() {
+    let registerBtns = document.getElementsByClassName('register-btn')
+    for (let i = 0; i < registerBtns.length; i++) {
+        registerBtns[i].addEventListener("click", () => {
+            registerBtns[i].innerHTML = 'Registered'
+            registerBtns[i].classList.add('registeredBtn')
+            let tableRow = document.createElement("td")
+            let studentData = document.getElementsByClassName('studentData')
+            tableRow.innerHTML = Date()
+            studentData[i].appendChild(tableRow)
+        }, { once: true })
+    }
+}
